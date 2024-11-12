382c19e13e02bb221599c92936906108285d8ab6
Ian Ndegwa <ndegwaian241@gmail.com>
2024-11-12 12:38:28 +0300

    message

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function submit(Request $request)
+    {
+        // validate the phone number
+        $request->validate([
+            'phone' => 'required|numeric|min10'
+        ]);
+
+        // find or create a user model
+        $user = User::firstOrCreate([
+            'phone' => $request->phone
+        ]);
+
+        if (!$user) {
+            return response()->json(['message' => 'could not process a user with that phone number.'], 401);
+        }
+
+        // send the user a one-time use code
+        
+    }
+}
