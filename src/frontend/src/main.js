10c3cafe608b6ca0d81beb9cd5f9f0d15f881349
Ian Ndegwa <ndegwaian241@gmail.com>
2024-11-29 12:23:11 +0300

    message

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,23 +1,23 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-import VueGoogleMaps from '@fawmi/vue-google-maps'
+import VueGoogleMaps from "@fawmi/vue-google-maps";
 
-import './assets/main.css'
+import "./assets/main.css";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
 app.use(VueGoogleMaps, {
-    load: {
-        key: 'AIzaSyCAqdwRPpTtDGc6lWZKlSO0EPgkAKRo-8o',
-        libraries: 'places'
-    }
-})
+  load: {
+    key: "AIzaSyCAqdwRPpTtDGc6lWZKlSO0EPgkAKRo-8o",
+    libraries: "places",
+  },
+});
 
-app.mount('#app')
+app.mount("#app");
